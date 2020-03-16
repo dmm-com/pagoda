c47c7870d1003617428b42336fb5a28ba7af3ba0
TAKANO Mitsuhiro <takano32@gmail.com>
2020-03-16 17:41:03 +0900

    [WIP] Multi-Database

--- airone/db_router.py
+++ airone/db_router.py
@@ -1,11 +1,14 @@
 import random
+import threading
 
 from django.conf import settings
 
-
 class DBRouter:
     def db_for_read(self, model, **hints):
-        return random.choice(settings.AIRONE['DB_SLAVES'])
+        setting = settings.AIRONE['DB_MASTER']
+        if DBSlaves.enable():
+            setting = random.choice(settings.AIRONE['DB_SLAVES'])
+        return setting
 
     def db_for_write(self, model, **hints):
         return settings.AIRONE['DB_MASTER']
@@ -15,3 +18,34 @@ class DBRouter:
 
     def allow_migrate(self, db, app_label, model=None, **hints):
         return True
+
+class DBType(Enum):
+    MASTER = 0
+    SLAVES = 1
+
+class DBSlaves():
+    dict = dict()
+    lock = threading.Lock()
+    def __init__(self):
+        self.dict = DBSlaves.dict
+        self.lock = DBSlaves.lock
+
+    def __enter__(self):
+        self.lock.acquire()
+        thread_id = threading.get_ident()
+        self.dict[thread_id] = DBType.SLAVES
+
+    def __exit__(self, *arg, **kwargs):
+        thread_id = threading.get_ident()
+        self.dict[thread_id] = DBType.MASTER
+        self.lock.release()
+
+    @classmethod
+    def enable(cls):
+        enable = False
+        thread_id = threading.get_ident()
+        if DBSlaves.dict[thread_id] == DBType.SLAVES:
+            enable = True
+        if DBSlaves.dict[thread_id] == DBType.MASTER:
+            enable = False
+        return enable
---
+++ airone/tests/test_multi_database.py
@@ -0,0 +1,16 @@
+from airone.lib.test import AironeTestCase
+from user.models import User
+
+class AirOneMuiltiDatabaseTest(AironeTestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_use_master(self):
+        self.assertTrue(True)
+
+    def test_use_slaves(self):
+        self.assertTrue(True)
