271dd45ae9ef443db57e8a18d720915188dc2f9d
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2020-11-26 23:58:19 +0900

    Update configuration file to be able to deploy documentation correctly

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
 #
-# This script is mainly used by CI to update contents of gh-pages to the latest one.
+# This script is mainly used by CI to update contents of gh-page to the latest one.
 #
+set -e
 
 if [ -z "${GH_TOKEN}" ]; then
   echo '(ERROR) The environment variables "GH_TOKEN" have to be set'
@@ -10,19 +11,19 @@ if [ -z "${GH_TOKEN}" ]; then
 fi
 
 # ${GH_TOKEN} is an access token of GitHub to update GitHub pages of the repository
-git clone -b gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
+git clone -b gh-page https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-page
 if [ $? -ne 0 ]; then
   echo "(ERROR) Failed to clone the remote repository from 'https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git'"
   exit 1
 fi
 
-cd gh-pages
+cd gh-page
 
 # clear old files and add new files
 rm -fR *
 cp -r ../docs/public/* ./
 
-# commit to remote repository on the gh-pages branch
+# commit to remote repository on the gh-page branch
 git add .
-git commit -m "Updates gh-pages because of updating the master branch."
-git push origin gh-pages
+git commit -m "Updates gh-page because of updating the master branch."
+git push origin gh-page
