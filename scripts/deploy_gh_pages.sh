6fd340958f5a48816bc665f02102e6ba62abebd1
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2020-11-30 00:02:06 +0900

    Fixed trivial mistake of specifying branch name

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# This script is mainly used by CI to update contents of gh-page to the latest one.
+# This script is mainly used by CI to update contents of gh-pages to the latest one.
 #
 set -e
 
@@ -11,19 +11,19 @@ if [ -z "${GH_TOKEN}" ]; then
 fi
 
 # ${GH_TOKEN} is an access token of GitHub to update GitHub pages of the repository
-git clone -b gh-page https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-page
+git clone -b gh-pages https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git gh-pages
 if [ $? -ne 0 ]; then
   echo "(ERROR) Failed to clone the remote repository from 'https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git'"
   exit 1
 fi
 
-cd gh-page
+cd gh-pages
 
 # clear old files and add new files
 rm -fR *
 cp -r ../docs/public/* ./
 
-# commit to remote repository on the gh-page branch
+# commit to remote repository on the gh-pages branch
 git add .
-git commit -m "Updates gh-page because of updating the master branch."
-git push origin gh-page
+git commit -m "Updates gh-pages because of updating the master branch."
+git push origin gh-pages
