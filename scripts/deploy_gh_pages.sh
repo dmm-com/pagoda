a33a01cc7a7a2c54d0ea8829dac9af454b93ef0d
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2020-11-30 20:08:23 +0900

    Added processing to set CNAME at deploying GitHub page contents

--- scripts/deploy_gh_pages.sh
+++ scripts/deploy_gh_pages.sh
@@ -24,6 +24,9 @@ cd gh-pages
 rm -fR * ./circleci
 cp -r ../docs/public/* ./
 
+# set custom domain for the GitHub Pages of this repository
+echo ${GH_CUSTOM_DOMAIN} > CNAME
+
 # commit to remote repository on the gh-pages branch
 git add .
 git commit -m "Updates gh-pages because of updating the master branch."
