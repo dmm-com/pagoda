aa7c174f5bd18ed1423a7491421f4c69aba2b631
TAKANO Mitsuhiro <takano32@gmail.com>
2021-01-04 15:05:37 +0900

    use User class with UserSocialAuth class

--- airone/views.py
+++ airone/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from user.models import User
 from social_django.models import UserSocialAuth
 
 def index(request):
-    social_user = UserSocialAuth.objects.get(user_id=request.user.id)
+    if User.objects.filter(id=request.user.id).exists():
+        user = User.objects.get(id=request.user.id)
+
+    if UserSocialAuth.objects.filter(user_id=request.user.id).exists():
+        social_user = UserSocialAuth.objects.get(user_id=request.user.id)
+
     return render(request, 'index.html')
