8e99bc343e0f71880c0d3e565810f9ab3905b94c
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-03-16 18:02:35 +0900

    Correcte the way of URL resolver to be complatible with Django v2.2

--- airone/urls.py
+++ airone/urls.py
@@ -8,20 +8,20 @@ from api_v1.urls import urlpatterns as api_v1_urlpatterns
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='dashboard/')),
-    url(r'^acl/', include('acl.urls', namespace='acl')),
-    url(r'^user/', include('user.urls', namespace='user')),
-    url(r'^group/', include('group.urls', namespace='group')),
-    url(r'^entity/', include('entity.urls', namespace='entity')),
-    url(r'^dashboard/', include('dashboard.urls', namespace='dashboard')),
-    url(r'^entry/', include('entry.urls', namespace='entry')),
+    url(r'^acl/', include(('acl.urls', 'acl'))),
+    url(r'^user/', include(('user.urls', 'user'))),
+    url(r'^group/', include(('group.urls', 'group'))),
+    url(r'^entity/', include(('entity.urls', 'entity'))),
+    url(r'^dashboard/', include(('dashboard.urls', 'dashboard'))),
+    url(r'^entry/', include(('entry.urls', 'entry'))),
     url(r'^admin/', admin.site.urls),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api-auth/', include(('rest_framework.urls', 'rest_framework'))),
     url(r'^api/v1/', include(api_v1_urlpatterns)),
-    url(r'^job/', include('job.urls', namespace='job')),
+    url(r'^job/', include(('job.urls', 'job'))),
 ]
 
 for extension in settings.AIRONE['EXTENSIONS']:
     urlpatterns.append(url(r'^extension/%s' % extension,
-                           include('%s.urls' % extension, namespace=extension)))
+                           include('%s.urls' % extension, extension)))
 
 urlpatterns += staticfiles_urlpatterns()
--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,8 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'template_name': 'logout.html'}, name='logout'),
     url(r'^import/$', views.import_data, name='import'),
     url(r'^do_import/$', views.do_import_data, name='do_import'),
     url(r'^advanced_search$', views.advanced_search, name='advanced_search'),
