712a78d75ff7960eb3164199502d69daaf9a6e71
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-04-23 11:56:48 +0900

    Fixed test associated with changing Django version

--- airone/tests/test_http.py
+++ airone/tests/test_http.py
@@ -18,12 +18,12 @@ class AirOneHTTPTest(unittest.TestCase):
 
         test_suites = [
             {'get_url': '/test',
-             'resp_url': ['/dashboard/login?next=/test?']},
+             'resp_url': ['/auth/login?next=/test?']},
             {'get_url': '/日本語',
-             'resp_url': ['/dashboard/login?next=/%E6%97%A5%E6%9C%AC%E8%AA%9E?']},
+             'resp_url': ['/auth/login?next=/%E6%97%A5%E6%9C%AC%E8%AA%9E?']},
             {'get_url': '/test?query1=1&query2=test',
-             'resp_url': ['/dashboard/login?next=/test?query1%3D1%26query2%3Dtest',
-                          '/dashboard/login?next=/test?query2%3Dtest%26query1%3D1']},
+             'resp_url': ['/auth/login?next=/test?query1%3D1%26query2%3Dtest',
+                          '/auth/login?next=/test?query2%3Dtest%26query1%3D1']},
         ]
 
         for i in test_suites:
