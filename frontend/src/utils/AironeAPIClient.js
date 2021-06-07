b4c8cfe54ccde0f01d52c1ae7c00cfc9488ecfd1
Ryo Okubo <syucream1031@gmail.com>
2021-06-07 19:32:00 +0900

    tmp

--- frontend/src/utils/AironeAPIClient.js
+++ frontend/src/utils/AironeAPIClient.js
@@ -8,6 +8,19 @@ export function getEntries(entityId) {
     return fetch(`/entry/api/v1/get_entries/${entityId}`);
 }
 
+export function getAdvancedSearchResults(entityIds, attributeIds, isAllEntities, hasReferral) {
+    let params = new URLSearchParams();
+    entityIds.forEach(id => params.append('entity', id));
+    attributeIds.forEach(id => params.append('attr', id));
+    params.append('is_all_entities', isAllEntities);
+    params.append('has_referral', hasReferral);
+
+    let url = new URL('/dashboard/advanced_search');
+    url.searchParams = params;
+
+    return fetch(url.toString());
+}
+
 // NOTE it calls non-API endpoint
 // FIXME implement internal API then call it
 export function createEntry(entityId, name, attrs) {
