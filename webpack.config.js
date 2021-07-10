e2bad9d99e10627d745b719c7a6cd9b9d52d7a02
Ryo Okubo <syucream1031@gmail.com>
2021-07-10 16:05:47 +0900

    Separate the form part on EditEntity

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,11 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
         }
       }
     ]
