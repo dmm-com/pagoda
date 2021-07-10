e46718e9bea65594f4b1774b01ef137a31d7e3d0
Ryo Okubo <syucream1031@gmail.com>
2021-07-10 16:30:08 +0900

    Introduce a fundamental test

---
+++ frontend/src/components/entity/EntityForm.test.js
@@ -0,0 +1,12 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import { render } from "@testing-library/react";
+
+import EntityForm from "./EntityForm";
+
+test("should render a component", function () {
+  expect(() => render(<EntityForm />)).not.toThrow();
+});
