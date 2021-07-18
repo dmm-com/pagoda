83cc829298a87c3f9469c58704eb123ac82dd227
Ryo Okubo <syucream1031@gmail.com>
2021-07-18 12:39:10 +0900

    Render jobs with response data from the backend

--- frontend/src/pages/Job.js
+++ frontend/src/pages/Job.js
@@ -16,6 +16,8 @@ import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import AironeBreadcrumbs from "../components/common/AironeBreadcrumbs";
 import { getJobs } from "../utils/AironeAPIClient";
+import { useAsync } from "react-use";
+import JobList from "../components/job/JobList";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -28,11 +30,8 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Job({}) {
   const classes = useStyles();
-  const [jobs, setJobs] = useState([]);
 
-  useEffect(() => {
-    getJobs().then((data) => setJobs(data));
-  }, []);
+  const jobs = useAsync(async () => getJobs().then((resp) => resp.json()), []);
 
   return (
     <div className="container-fluid">
@@ -58,83 +57,7 @@ export default function Job({}) {
         </div>
       </div>
 
-      <TableContainer component={Paper}>
-        <Table>
-          <TableHead>
-            <TableRow>
-              <TableCell>
-                <Typography>対象エントリ</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>操作</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>状況</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>実行時間</Typography>
-              </TableCell>
-              <TableCell>
-                <Typography>実行日時</Typography>
-              </TableCell>
-              <TableCell align="right">
-                <Typography>備考</Typography>
-              </TableCell>
-              <TableCell align="right" />
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {jobs.map((job) => (
-              <TableRow>
-                <TableCell>
-                  <Typography>{job.entry}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{job.operation}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{job.status}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{job.duration}</Typography>
-                </TableCell>
-                <TableCell>
-                  <Typography>{job.created_at}</Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <Typography>{job.note}</Typography>
-                </TableCell>
-                <TableCell align="right">
-                  <List>
-                    <ListItem>
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        className={classes.button}
-                        component={Link}
-                        to={`/jobs/${job.id}/rerun`}
-                      >
-                        Re-run
-                      </Button>
-                    </ListItem>
-                    <ListItem>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        className={classes.button}
-                        component={Link}
-                        to={`/jobs/${job.id}/cancel`}
-                      >
-                        Cancel
-                      </Button>
-                    </ListItem>
-                  </List>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      {!jobs.loading && <JobList jobs={jobs.value} />}
     </div>
   );
 }
