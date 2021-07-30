b4dad0feb8ff9c22e674670d14c27d9f64fdfd40
TAKANO Mitsuhiro <takano32@gmail.com>
2021-07-30 17:02:26 +0900

    fix for PEP8

--- airone/views.py
+++ airone/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from user.models import User
 from social_django.models import UserSocialAuth
 
+
 def index(request):
+    user = social_user = None
     if User.objects.filter(id=request.user.id).exists():
         user = User.objects.get(id=request.user.id)
 
     if UserSocialAuth.objects.filter(user_id=request.user.id).exists():
         social_user = UserSocialAuth.objects.get(user_id=request.user.id)
 
+    [user, social_user]
     return render(request, 'index.html')
