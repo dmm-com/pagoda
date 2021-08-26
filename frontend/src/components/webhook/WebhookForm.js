b300199672fe64b8f26a400363982e73f1918539
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-08-26 15:32:56 +0900

    Added Modal for creating Webhook registration

--- frontend/src/components/webhook/WebhookForm.js
+++ frontend/src/components/webhook/WebhookForm.js
@@ -2,10 +2,26 @@ import { makeStyles } from "@material-ui/core/styles";
 import React, { useState } from "react";
 import Button from "@material-ui/core/Button";
 import PropTypes from "prop-types";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
 
 const useStyles = makeStyles((theme) => ({
   button: {
     margin: theme.spacing(1),
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+    width: '80%',
   },
 }));
 
@@ -13,16 +29,40 @@ export default function WebhookForm({
 }) {
   const classes = useStyles();
 
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpenModal = () => {
+    setOpen(true);
+  }
+
+  const handleCloseModal = () => {
+    setOpen(false);
+  }
+
   return (
     <div>
       <Button
         className={classes.button}
+        onClick={handleOpenModal}
         type="submit"
         variant="contained"
         color="secondary"
       >
         Add Webhook
       </Button>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleCloseModal}
+      >
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
+      </Modal>
     </div>
   );
 }
