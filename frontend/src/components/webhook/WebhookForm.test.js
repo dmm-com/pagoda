2e5e5ea45b2ea032e0a9203919de70e4b3968d2d
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-09-21 16:36:02 +0900

    Refacotored test of WebhookForm component

--- frontend/src/components/webhook/WebhookForm.test.js
+++ frontend/src/components/webhook/WebhookForm.test.js
@@ -2,11 +2,26 @@
  * @jest-environment jsdom
  */
 
-import React from "react";
-import { render } from "@testing-library/react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
 
 import WebhookForm from "./WebhookForm.js";
 
+let container;
+
+beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+});
+
+afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+});
+
 test("should render a component with essential props", function () {
-  expect(() => render(<WebhookForm entityId={0} />)).not.toThrow();
+  expect(() => {
+    ReactDOM.render(<WebhookForm entityId={0} />, container);
+  }).not.toThrow();
 });
