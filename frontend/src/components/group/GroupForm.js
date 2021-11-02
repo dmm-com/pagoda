6b1e5f85450cb0e79a0f3a1756305ba57faab9b3
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2021-11-02 14:49:45 +0900

    Refactored GroupForm from function component to class one
    Enzyme, which is another react testing framework, has a restriction to
    check state variable of component that target component is implemented
    by class component. So this commit refactored it form function component
    to class component.

--- frontend/src/components/group/GroupForm.js
+++ frontend/src/components/group/GroupForm.js
@@ -1,84 +1,101 @@
 import { Select } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 import PropTypes from "prop-types";
-import React, { useState } from "react";
-import { useHistory } from "react-router-dom";
+import React from "react";
+import { withRouter } from "react-router-dom";
 
 import { groupsPath } from "../../Routes";
 import { createGroup, updateGroup } from "../../utils/AironeAPIClient";
 
-const useStyles = makeStyles((theme) => ({
-  button: {
-    margin: theme.spacing(1),
-  },
-}));
+export class GroupForm extends React.Component {
+  constructor(props) {
+    super(props);
 
-export function GroupForm({ users, group }) {
-  const classes = useStyles();
-  const history = useHistory();
+    this.state = {
+      name: props.group?.name ?? "",
+      members: props.group?.members ? props.group?.members.map((m) => m.id) : [],
+    };
+  }
 
-  const [name, setName] = useState(group?.name ?? "");
-  const [members, setMembers] = useState(group?.members.map((m) => m.id) ?? []);
-
-  const handleChangeSelectedOptions = (event) => {
+  handleChangeSelectedOptions = (event) => {
     const options = Array.from(event.target.options, (o) => o);
     const values = options.filter((o) => o.selected).map((o) => o.value);
-    setMembers(values);
+
+    this.setState({ members: values });
   };
 
-  const handleSubmit = (event) => {
-    if (group.id !== undefined) {
-      updateGroup(group.id, name, members).then(() =>
-        history.replace(groupsPath())
-      );
+  handleSubmit = (event) => {
+    const { group, history } = this.props;
+    const { name, members } = this.state;
+
+    if (group && group.id !== undefined) {
+      updateGroup(group.id, name, members).then(() => {
+        history.replace(groupsPath());
+      });
     } else {
-      createGroup(name, members).then(() => history.replace(groupsPath()));
+      createGroup(name, members).then(() => {
+        history.replace(groupsPath())
+      });
     }
 
     event.preventDefault();
   };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <Typography>グループ名</Typography>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required="required"
-        />
-      </div>
-      <div>
-        <Typography>ユーザ管理</Typography>
-        <Select
-          multiple
-          native
-          variant="outlined"
-          value={members}
-          onChange={handleChangeSelectedOptions}
+  render() {
+    const { users, history, classes } = this.props;
+    const { name, members } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit.bind(this)}>
+        <div>
+          <Typography>グループ名 (tmp02)</Typography>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={(e) => this.setState({ name: e.target.value })}
+            required="required"
+          />
+        </div>
+        <div>
+          <Typography>ユーザ管理</Typography>
+          <Select
+            multiple
+            native
+            variant="outlined"
+            value={members}
+            onChange={this.handleChangeSelectedOptions.bind(this)}
+          >
+            {users.map((user) => (
+              <option key={user.id} value={user.id}>
+                {user.username}
+              </option>
+            ))}
+          </Select>
+        </div>
+        <Button
+          //className={classes.button}
+          type="submit"
+          variant="contained"
+          color="secondary"
         >
-          {users.map((user) => (
-            <option key={user.id} value={user.id}>
-              {user.username}
-            </option>
-          ))}
-        </Select>
-      </div>
-      <Button
-        className={classes.button}
-        type="submit"
-        variant="contained"
-        color="secondary"
-      >
-        保存
-      </Button>
-    </form>
-  );
+          保存
+        </Button>
+      </form>
+    );
+  }
 }
+
+const styles = (theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+});
+
+//export class GroupForm = withRouter(withStyles(styles)(_GroupForm));
+withRouter(withStyles(styles)(GroupForm));
 
 GroupForm.propTypes = {
   users: PropTypes.arrayOf(
@@ -88,13 +105,13 @@ GroupForm.propTypes = {
     })
   ).isRequired,
   group: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string.isRequired,
+    id: PropTypes.number,
+    name: PropTypes.string,
     members: PropTypes.arrayOf(
       PropTypes.shape({
-        id: PropTypes.number,
-        username: PropTypes.string,
+        id: PropTypes.number.isRequired,
+        username: PropTypes.string.isRequired,
       })
-    ).isRequired,
+    ),
   }),
 };
