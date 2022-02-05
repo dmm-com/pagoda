477685d3d11e3357bc3b2167312a98871341eb5c
Ryo Okubo <syucream1031@gmail.com>
2022-02-05 11:12:16 +0900

    Confirm before deleting

--- frontend/src/components/entry/EntryList.tsx
+++ frontend/src/components/entry/EntryList.tsx
@@ -25,6 +25,7 @@ import { Link, useHistory } from "react-router-dom";
 import { aclPath, newEntryPath, showEntryPath } from "../../Routes";
 import { deleteEntry, restoreEntry } from "../../utils/AironeAPIClient";
 import { EntryList as ConstEntryList } from "../../utils/Constants";
+import { Confirmable } from "../common/Confirmable";
 
 const useStyles = makeStyles<Theme>((theme) => ({
   button: {
@@ -71,10 +72,15 @@ const EntryControlMenu: FC<EntryControlProps> = ({
       <MenuItem component={Link} to={showEntryPath(entryId)}>
         <Typography>編集</Typography>
       </MenuItem>
-      {/* have a confirmable component */}
-      <MenuItem onClick={(e) => handleDelete(e, entryId)}>
-        <Typography>削除</Typography>
-      </MenuItem>
+      <Confirmable
+        componentGenerator={(handleOpen) => (
+          <MenuItem onClick={handleOpen}>
+            <Typography>削除</Typography>
+          </MenuItem>
+        )}
+        dialogTitle="本当に削除しますか？"
+        onClickYes={(e) => handleDelete(e, entryId)}
+      />
       {/* This is a temporary configuration until
           Entry's edit page will be divided from showing Page */}
       <MenuItem component={Link} to={aclPath(entryId)}>
