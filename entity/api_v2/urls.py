25fef635e4863bd3d0664003841c82c6e86935ec
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2022-02-09 11:25:22 +0900

    Removed redundant code

--- entity/api_v2/urls.py
+++ entity/api_v2/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^history/(\d+)$', views.history, name='history'),
-    url(r'^entities/(\d+)$', views.get_entity, name='get_entity'),
     url(r'^(?P<pk>\d+)$', views.GetEntity.as_view({'get': 'retrieve'})),
 ]
--- entity/api_v2/views.py
+++ entity/api_v2/views.py
@@ -38,34 +38,6 @@ def history(request, entity_id):
     } for h in histories], safe=False)
 
 
-def get_entity(request, entity_id):
-    user = User.objects.get(id=request.user.id)
-
-    if not Entity.objects.filter(id=entity_id).exists():
-        return HttpResponse('Failed to get entity of specified id', status=400)
-
-    # entity to be editted is given by url
-    entity = Entity.objects.get(id=entity_id)
-
-    return JsonResponse({
-        'name': entity.name,
-        'note': entity.note,
-        'is_toplevel': (entity.status & Entity.STATUS_TOP_LEVEL) != 0,
-        'attributes': [{
-            'id': x.id,
-            'name': x.name,
-            'type': x.type,
-            'is_mandatory': x.is_mandatory,
-            'is_delete_in_chain': x.is_delete_in_chain,
-            'referrals': [{
-                'id': r.id,
-                'name': r.name,
-            } for r in x.referral.all()],
-        } for x in entity.attrs.filter(is_active=True).order_by('index')
-            if user.has_permission(x, ACLType.Writable)],
-    })
-
-
 class GetEntity(viewsets.ReadOnlyModelViewSet):
     queryset = Entity.objects.filter(is_active=True)
     serializer_class = EntitySerializer
