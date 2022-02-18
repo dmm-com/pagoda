a43eb6cb3e3116a702055f1e4cfe55519b41175f
hinashi <k.hinagawa@gmail.com>
2022-02-18 16:56:51 +0900

    Added scroll anchor link in entry details page

--- frontend/src/components/entry/EntryDetails.tsx
+++ frontend/src/components/entry/EntryDetails.tsx
@@ -1,5 +1,6 @@
 import { Box, Grid, Typography } from "@mui/material";
 import React, { FC } from "react";
+import { useLocation } from "react-router-dom";
 
 import { EntryAttributes } from "components/entry/EntryAttributes";
 import { EntryReferral } from "components/entry/EntryReferral";
@@ -10,6 +11,21 @@ interface Props {
 }
 
 export const EntryDetails: FC<Props> = ({ entry, referredEntries }) => {
+  const location = useLocation();
+
+  const refAttrList = React.createRef<HTMLSpanElement>();
+
+  React.useEffect(() => {
+    console.log("location.hash", location.hash);
+    switch (location.hash) {
+      case "#attr_list":
+        refAttrList.current.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+    }
+  }, [location.hash]);
+
   return (
     <Grid
       container
@@ -30,7 +46,7 @@ export const EntryDetails: FC<Props> = ({ entry, referredEntries }) => {
       </Grid>
       <Grid item xs={4}>
         <Box p="32px">
-          <Typography p="32px" fontSize="32px" align="center">
+          <Typography p="32px" fontSize="32px" align="center" ref={refAttrList}>
             項目一覧
           </Typography>
           <EntryAttributes attributes={entry.attrs} />
