dbdb5e8f464d7c950d7566a249c1a1fd3a87289f
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2022-05-02 17:49:25 +0900

    Added comments to be understandable when it's reviewed at a later date

--- airone/spectacular.py
+++ airone/spectacular.py
@@ -1,4 +1,11 @@
+# These methods are used for filtering API endpoints to generate
+# OpenAPI schema using drf-spectacular.
+# (c.f. https://drf-spectacular.readthedocs.io/en/latest/settings.html)
+
 def exclude_customview_hook(endpoints):
+    """This excludes API endpoints of custom-view's ones. Because it's not always
+    necessary to generage custom-view's OpenAPI schema.
+    """
     result = []
     for (path, path_regex, method, callback) in endpoints:
         if "/custom/" not in path:
@@ -7,6 +14,9 @@ def exclude_customview_hook(endpoints):
 
 
 def filter_apiv2_hook(endpoints):
+    """This excludes API endpoints for AirOne API(v1). React views refer to only
+    AirOne API(v2). So it's not necessary to generate API(v1)'s OpenAPI schema.
+    """
     result = []
     for (path, path_regex, method, callback) in endpoints:
         if "/api/v2/" in path:
