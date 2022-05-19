f61e075e9a44a2b4b1d4f065fcc67dfa5c447d09
hinashi <k.hinagawa@gmail.com>
2022-05-19 09:36:25 +0900

    Added create airone user in social_auth

---
+++ airone/auth/social_auth.py
@@ -0,0 +1,18 @@
+from user.models import User
+
+
+def create_user(details, user=None, *args, **kwargs):
+
+    # When first time login, UserSocialAuth model's object is created.
+    # If UserSocialAuth exists, user already exists.
+    if user:
+        return {"is_new": False}
+
+    # Returns User if the AirOne user already exists.
+    user = User.objects.filter(username=details["username"], is_active=True).first()
+    if user:
+        return {"user": user}
+
+    # Create a User if the AirOne user does not exist.
+    user = User.objects.create(username=details["username"], email=details["email"])
+    return {"user": user}
--- airone/tests/test_auth.py
+++ airone/tests/test_auth.py
@@ -1,10 +1,13 @@
 from airone.lib.test import AironeViewTest
+from airone.auth.social_auth import create_user
 from django.conf import settings
+from user.models import User
 
 settings.AIRONE["TITLE"] = "TITLE"
 settings.AIRONE["SUBTITLE"] = "SUBTITLE"
 settings.AIRONE["NOTE_DESC"] = "NOTE_DESC"
 settings.AIRONE["NOTE_LINK"] = "NOTE_LINK"
+settings.AIRONE["SSO_DESC"] = "SSO_DESC"
 
 
 class ViewTest(AironeViewTest):
@@ -14,6 +17,7 @@ class ViewTest(AironeViewTest):
         self.assertEqual(resp.context["subtitle"], "SUBTITLE")
         self.assertEqual(resp.context["note_desc"], "NOTE_DESC")
         self.assertEqual(resp.context["note_link"], "NOTE_LINK")
+        self.assertEqual(resp.context["sso_desc"], "SSO_DESC")
 
     def test_logout_with_get(self):
         self.guest_login()
@@ -26,3 +30,17 @@ class ViewTest(AironeViewTest):
         resp = self.client.post("/auth/logout/")
         self.assertEqual(resp.status_code, 200)
         self.assertTemplateUsed(template_name="registration/logged_out.html")
+
+    def test_social_auth_create_user(self):
+        user = User.objects.create(username="hogefuga")
+
+        result = create_user({}, user)
+        self.assertEqual(result["is_new"], False)
+
+        result = create_user({"username": "hogefuga", "email": "hogefuga@example.com"})
+        self.assertEqual(result["user"], user)
+
+        user.delete()
+        result = create_user({"username": "hogefuga", "email": "hogefuga@example.com"})
+        new_user = User.objects.filter(username="hogefuga").first()
+        self.assertEqual(result["user"], new_user)
