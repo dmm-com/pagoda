9777e141f72b9d997fc52ce882091f2557a8acab
hinashi <k.hinagawa@gmail.com>
2022-05-20 18:03:25 +0900

    Fixed a minor issue

--- airone/auth/social_auth.py
+++ airone/auth/social_auth.py
@@ -10,9 +10,8 @@ def create_user(details, user=None, *args, **kwargs):
 
     # Returns User if the AirOne user already exists.
     user = User.objects.filter(username=details["username"], is_active=True).first()
-    if user:
-        return {"user": user}
+    if not user:
+        # Create a User if the AirOne user does not exist.
+        user = User.objects.create(username=details["username"], email=details["email"])
 
-    # Create a User if the AirOne user does not exist.
-    user = User.objects.create(username=details["username"], email=details["email"])
     return {"user": user}
