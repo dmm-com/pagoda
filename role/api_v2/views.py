c9dff16929a32c4055310e665c975254f5add2bd
Ryo Okubo <syucream1031@gmail.com>
2022-10-08 15:38:30 +0900

    Fix format

--- role/api_v2/views.py
+++ role/api_v2/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
-from role.api_v2.serializers import RoleSerializer, RoleCreateUpdateSerializer
+from role.api_v2.serializers import RoleCreateUpdateSerializer, RoleSerializer
 from role.models import Role
 
 
