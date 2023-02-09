1324fee246f04aa928138987e5f7d1fc5c2e7c7b
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2023-02-09 14:48:28 +0900

    Fixed a bug not to be able to move correct page after creating Entity

--- frontend/src/pages/EditEntityPage.tsx
+++ frontend/src/pages/EditEntityPage.tsx
@@ -148,7 +148,11 @@ export const EditEntityPage: FC = () => {
   // by handleSubmit() processing to prevent showing wrong Prompt message.
   useEffect(() => {
     if (submitted) {
-      history.replace(entityEntriesPath(entityId));
+      if (createMode) {
+        history.replace(entitiesPath());
+      } else {
+        history.replace(entityEntriesPath(entityId));
+      }
     }
   }, [submitted]);
 
