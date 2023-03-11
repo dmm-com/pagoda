e275975a5f7c65f93845df949a3dcd83a0180820
Ryo Okubo <syucream1031@gmail.com>
2023-03-11 22:13:44 +0900

    Add a test for client side validations

---
+++ frontend/src/components/entity/EntityFormSchema.test.ts
@@ -0,0 +1,108 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { AttributeTypes } from "../../services/Constants";
+
+import { Schema, schema } from "./EntityFormSchema";
+
+describe("schema", () => {
+  const baseValue: Schema = {
+    name: "entity",
+    note: "note",
+    isToplevel: false,
+    webhooks: [
+      {
+        url: "https://example.com/",
+        label: "label",
+        isEnabled: false,
+        isVerified: false,
+        headers: [
+          {
+            headerKey: "key1",
+            headerValue: "value1",
+          },
+        ],
+        isDeleted: false,
+      },
+    ],
+    attrs: [
+      {
+        name: "attr1",
+        type: AttributeTypes.string.type,
+        isMandatory: false,
+        isDeleteInChain: false,
+        isDeleted: false,
+        referral: [
+          {
+            id: 1,
+            name: "referred1",
+          },
+        ],
+        index: 0,
+        isSummarized: false,
+      },
+    ],
+  };
+
+  test("validation succeeds for a valid value", () => {
+    const value = { ...baseValue };
+    expect(schema.parse(value)).toEqual(value);
+  });
+
+  test("validation fails if name is empty", () => {
+    const value = {
+      ...baseValue,
+      name: "",
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if webhook url is invalid format", () => {
+    const value = {
+      ...baseValue,
+      webhooks: [
+        {
+          ...baseValue.webhooks[0],
+          url: "invalid url",
+        },
+      ],
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if webhook headers headerKey is empty", () => {
+    const value = {
+      ...baseValue,
+      webhooks: [
+        {
+          ...baseValue.webhooks[0],
+          headers: [
+            {
+              ...baseValue.webhooks[0].headers[0],
+              headerKey: "",
+            },
+          ],
+        },
+      ],
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if attr name is empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: [
+        {
+          ...baseValue.attrs[0],
+          name: "",
+        },
+      ],
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+});
