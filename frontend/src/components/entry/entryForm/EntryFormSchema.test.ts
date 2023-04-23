2e7c8afbe3836f5d39a89e553687416a07662806
Ryo Okubo <syucream1031@gmail.com>
2023-04-23 16:09:46 +0900

    Implement isMandatory value validation

--- frontend/src/components/entry/entryForm/EntryFormSchema.test.ts
+++ frontend/src/components/entry/entryForm/EntryFormSchema.test.ts
@@ -2,13 +2,11 @@
  * @jest-environment jsdom
  */
 
-import { DjangoContext } from "../../../services/DjangoContext";
+import { AttributeTypes } from "../../../services/Constants";
 
 import { schema, Schema } from "./EntryFormSchema";
 
 describe("schema", () => {
-  const djangoContext = DjangoContext.getInstance();
-
   const baseValue: Schema = {
     name: "entry",
     schema: {
@@ -17,7 +15,7 @@ describe("schema", () => {
     },
     attrs: {
       string: {
-        type: djangoContext?.attrTypeValue.string ?? 0,
+        type: AttributeTypes.string.type,
         isMandatory: false,
         schema: {
           id: 1,
@@ -25,6 +23,145 @@ describe("schema", () => {
         },
         value: {
           asString: "string",
+        },
+      },
+      arrayString: {
+        type: AttributeTypes.array_string.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "array_string",
+        },
+        value: {
+          asArrayString: ["string"],
+        },
+      },
+      object: {
+        type: AttributeTypes.object.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "object",
+        },
+        value: {
+          asObject: {
+            id: 1,
+            name: "object",
+            _boolean: false,
+          },
+        },
+      },
+      arrayObject: {
+        type: AttributeTypes.array_object.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "array_object",
+        },
+        value: {
+          asArrayObject: [
+            {
+              id: 1,
+              name: "object",
+              _boolean: false,
+            },
+          ],
+        },
+      },
+      namedObject: {
+        type: AttributeTypes.named_object.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "named_object",
+        },
+        value: {
+          asNamedObject: {
+            name: {
+              id: 1,
+              name: "object",
+              _boolean: false,
+            },
+          },
+        },
+      },
+      arrayNamedObject: {
+        type: AttributeTypes.array_named_object.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "array_named_object",
+        },
+        value: {
+          asArrayNamedObject: [
+            {
+              name: {
+                id: 1,
+                name: "object",
+                _boolean: false,
+              },
+            },
+          ],
+        },
+      },
+      group: {
+        type: AttributeTypes.group.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "group",
+        },
+        value: {
+          asGroup: {
+            id: 1,
+            name: "group",
+          },
+        },
+      },
+      arrayGroup: {
+        type: AttributeTypes.array_group.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "array_group",
+        },
+        value: {
+          asArrayGroup: [
+            {
+              id: 1,
+              name: "group",
+            },
+          ],
+        },
+      },
+      role: {
+        type: AttributeTypes.role.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "role",
+        },
+        value: {
+          asRole: {
+            id: 1,
+            name: "role",
+          },
+        },
+      },
+      arrayRole: {
+        type: AttributeTypes.array_role.type,
+        isMandatory: false,
+        schema: {
+          id: 1,
+          name: "role",
+        },
+        value: {
+          asArrayRole: [
+            {
+              id: 1,
+              name: "role",
+            },
+          ],
         },
       },
     },
@@ -39,6 +176,176 @@ describe("schema", () => {
     const value = {
       ...baseValue,
       name: "",
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if string attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        string: {
+          ...baseValue.attrs.string,
+          isMandatory: true,
+          value: {
+            asString: "",
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if array-string attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        arrayString: {
+          ...baseValue.attrs.arrayString,
+          isMandatory: true,
+          value: {
+            asArrayString: [],
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if object attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        object: {
+          ...baseValue.attrs.object,
+          isMandatory: true,
+          value: {
+            asObject: null,
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if array-object attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        object: {
+          ...baseValue.attrs.object,
+          isMandatory: true,
+          value: {
+            asArrayObject: [null],
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if named-object attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        namedObject: {
+          ...baseValue.attrs.namedObject,
+          isMandatory: true,
+          value: {
+            asNamedObject: { name: null },
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if array-named-object attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        arrayNamedObject: {
+          ...baseValue.attrs.arrayNamedObject,
+          isMandatory: true,
+          value: {
+            asArrayNamedObject: [{ name: null }],
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if group attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        group: {
+          ...baseValue.attrs.group,
+          isMandatory: true,
+          value: {
+            asGroup: null,
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if array-group attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        arrayGroup: {
+          ...baseValue.attrs.arrayGroup,
+          isMandatory: true,
+          value: {
+            asArrayGroup: [null],
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if role attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        role: {
+          ...baseValue.attrs.role,
+          isMandatory: true,
+          value: {
+            asRole: null,
+          },
+        },
+      },
+    };
+
+    expect(() => schema.parse(value)).toThrow();
+  });
+
+  test("validation fails if array-role attr value is mandatory and empty", () => {
+    const value = {
+      ...baseValue,
+      attrs: {
+        arrayRole: {
+          ...baseValue.attrs.arrayRole,
+          isMandatory: true,
+          value: {
+            asArrayRole: [null],
+          },
+        },
+      },
     };
 
     expect(() => schema.parse(value)).toThrow();
