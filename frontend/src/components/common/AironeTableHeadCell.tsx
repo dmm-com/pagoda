8fecae7e3b752a13886c5494ebdca6a7a03b9897
Ryo Okubo <syucream1031@gmail.com>
2023-05-27 20:20:38 +0900

    Refactoring joblist

---
+++ frontend/src/components/common/AironeTableHeadCell.tsx
@@ -0,0 +1,6 @@
+import { TableCell } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const AironeTableHeadCell = styled(TableCell)(({}) => ({
+  color: "#FFFFFF",
+}));
---
+++ frontend/src/components/common/AironeTableHeadRow.tsx
@@ -0,0 +1,6 @@
+import { TableRow } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const AironeTableHeadRow = styled(TableRow)(({}) => ({
+  backgroundColor: "#455A64",
+}));
