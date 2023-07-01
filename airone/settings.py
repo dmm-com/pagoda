1d3f10eab6057203043e13253567ab801935ad12
Ryo Okubo <syucream1031@gmail.com>
2023-07-02 01:43:59 +0900

    Ignore type error on a value in dict

--- airone/settings.py
+++ airone/settings.py
@@ -13,4 +13,5 @@ class Prd(Common):
 
 class DRFSpectacularCustomView(Common):
     SPECTACULAR_SETTINGS = deepcopy(Common.SPECTACULAR_SETTINGS)
-    SPECTACULAR_SETTINGS["PREPROCESSING_HOOKS"].remove("airone.spectacular.exclude_customview_hook")
+    SPECTACULAR_SETTINGS["PREPROCESSING_HOOKS"]\
+        .remove("airone.spectacular.exclude_customview_hook")  # type: ignore
