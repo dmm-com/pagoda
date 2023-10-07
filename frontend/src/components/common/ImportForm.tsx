a8e3d100f1c9be074a616ab1e685618c1a215e57
Ryo Okubo <syucream1031@gmail.com>
2023-10-07 19:40:54 +0900

    Enable to force-import entries to pass backend limitation explicitly

--- frontend/src/components/common/ImportForm.tsx
+++ frontend/src/components/common/ImportForm.tsx
@@ -32,6 +32,7 @@ export const ImportForm: FC<Props> = ({ handleImport, handleCancel }) => {
           await handleImport(fileReader.result);
           history.go(0);
         } catch (e) {
+          // TODO check the error code returned from the backend to get error details
           setErrorMessage("ファイルのアップロードに失敗しました。");
           enqueueSnackbar("ファイルのアップロードに失敗しました", {
             variant: "error",
@@ -42,32 +43,30 @@ export const ImportForm: FC<Props> = ({ handleImport, handleCancel }) => {
   };
 
   return (
-    <Box>
-      <Box display="flex" flexDirection="column">
-        <Input type="file" onChange={onChange} />
+    <Box display="flex" flexDirection="column">
+      <Input type="file" onChange={onChange} />
 
-        <Typography color="error" variant="caption" my="4px">
-          {errorMessage}
-        </Typography>
-        <Box display="flex" justifyContent="flex-end">
-          <Button
-            type="submit"
-            variant="contained"
-            color="secondary"
-            onClick={onClick}
-            sx={{ m: "4px" }}
-          >
-            インポート
-          </Button>
-          <Button
-            variant="contained"
-            color="info"
-            onClick={handleCancel}
-            sx={{ m: "4px" }}
-          >
-            キャンセル
-          </Button>
-        </Box>
+      <Typography color="error" variant="caption" my="4px">
+        {errorMessage}
+      </Typography>
+      <Box display="flex" justifyContent="flex-end">
+        <Button
+          type="submit"
+          variant="contained"
+          color="secondary"
+          onClick={onClick}
+          sx={{ m: "4px" }}
+        >
+          インポート
+        </Button>
+        <Button
+          variant="contained"
+          color="info"
+          onClick={handleCancel}
+          sx={{ m: "4px" }}
+        >
+          キャンセル
+        </Button>
       </Box>
     </Box>
   );
