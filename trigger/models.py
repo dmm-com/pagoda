b4b27960fa9b5a9cf49ace389243ba96d577831e
hinashi <k.hinagawa@gmail.com>
2023-11-16 09:01:51 +0000

    Added register action logic

--- trigger/models.py
+++ trigger/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-from airone.lib.types import AttrTypeValue
+
 from airone.exceptions.trigger import InvalidInputException
+from airone.lib.types import AttrTypeValue
 from entity.models import Entity, EntityAttr
 from entry.models import Entry
 
 
 ## These are internal classes for AirOne trigger and action
-class InputAironeTriggerCondition(object):
+class InputTriggerCondition(object):
     def __init__(self, **input):
         # set EntityAttr from "attr" parameter of input
         attr_id = input.get("attr_id", 0)
@@ -26,10 +27,11 @@ class InputAironeTriggerCondition(object):
         self.bool_cond = input.get("bool_cond", False)
 
 
-class InputAironeTriggerActionValue(object):
+class InputTriggerActionValue(object):
     def __init__(self, **input):
         self.str_cond = input.get("str_cond", "")
         ref_id = input.get("ref_cond", 0)
+        self.ref_cond = None
         if ref_id:
             self.ref_cond = Entry.objects.filter(id=ref_id, is_active=True).first()
             if not self.ref_cond:
@@ -38,17 +40,17 @@ class InputAironeTriggerActionValue(object):
         self.bool_cond = input.get("bool_cond", False)
 
 
-class InputAironeTriggerAction(object):
+class InputTriggerAction(object):
     def __init__(self, **input):
         # set EntityAttr from "attr" parameter of input
-        attr_id = input.get("attr", 0)
+        attr_id = input.get("attr_id", 0)
         self.attr = EntityAttr.objects.filter(id=attr_id, is_active=True).first()
         if not self.attr:
             raise InvalidInputException("Specified attr(%s) is invalid" % attr_id)
 
-        value = self.get_value(input.get("values"))
-        if not value:
-            raise InvalidInputException("There is no action value")
+        value = self.get_value(input.get("value"))
+        if value is None:
+            self.values = []
         elif isinstance(value, list):
             self.values = value
         else:
@@ -56,16 +58,15 @@ class InputAironeTriggerAction(object):
 
     def get_value(self, input_value):
         if isinstance(input_value, str):
-            return InputAironeTriggerActionValue(str_cond=input_value)
+            return InputTriggerActionValue(str_cond=input_value)
         elif isinstance(input_value, Entry):
-            return InputAironeTriggerActionValue(ref_cond=input_value)
+            return InputTriggerActionValue(ref_cond=input_value)
         elif isinstance(input_value, dict):
-            return InputAironeTriggerActionValue(str_cond=input_value["name"], ref_cond=input_value["id"])
+            return InputTriggerActionValue(str_cond=input_value["name"], ref_cond=input_value["id"])
         elif isinstance(input_value, bool):
-            return InputAironeTriggerActionValue(bool_cond=input_value)
+            return InputTriggerActionValue(bool_cond=input_value)
         elif isinstance(input_value, list):
             return [self.get_value(x) for x in input_value if x]
-
 
     def save_actions(self):
         pass
@@ -74,12 +75,12 @@ class InputAironeTriggerAction(object):
 class TriggerParentCondition(models.Model):
     entity = models.ForeignKey("entity.Entity", on_delete=models.CASCADE)
 
-    def is_match_condition(self, inputs: list[InputAironeTriggerCondition]):
+    def is_match_condition(self, inputs: list[InputTriggerCondition]):
         if all([c.is_same_condition(inputs) for c in self.co_condition.all()]):
             return True
         return False
 
-    def save_conditions(self, inputs: list[InputAironeTriggerCondition]):
+    def save_conditions(self, inputs: list[InputTriggerCondition]):
         for input_cond in inputs:
             params = {
                 "parent": self,
@@ -94,16 +95,16 @@ class TriggerParentCondition(models.Model):
 
 class TriggerCondition(models.Model):
     parent = models.ForeignKey(
-        TriggerParentCondition, on_delete=models.CASCADE, related_name="co_condition"
+        TriggerParentCondition, on_delete=models.CASCADE, related_name="co_conditions"
     )
     attr = models.ForeignKey("entity.EntityAttr", on_delete=models.CASCADE)
     str_cond = models.TextField()
     ref_cond = models.ForeignKey("entry.Entry", on_delete=models.SET_NULL, null=True, blank=True)
     bool_cond = models.BooleanField(default=False)
 
-    def is_same_condition(self, input_list: list[InputAironeTriggerCondition]) -> bool:
-        # This checks one of the InputAirOneCondition which is in input_list matches with this condition
-        def _do_check_condition(input: InputAironeTriggerCondition):
+    def is_same_condition(self, input_list: list[InputTriggerCondition]) -> bool:
+        # This checks one of the InputCondition which is in input_list matches with this condition
+        def _do_check_condition(input: InputTriggerCondition):
             if self.attr.id == input.attr.id:
                 if self.attr.type & AttrTypeValue["boolean"]:
                     return self.bool_cond == input.bool_cond
@@ -120,40 +121,53 @@ class TriggerCondition(models.Model):
 
     @classmethod
     def register(cls, entity: Entity, conditions: list, actions: list):
-        # convert input to InputAironeTriggerCondition
-        input_trigger_conditions = [InputAironeTriggerCondition(**condition) for condition in conditions]
+        # convert input to InputTriggerCondition
+        input_trigger_conditions = [InputTriggerCondition(**condition) for condition in conditions]
 
         # check if condition is already registered
         for parent_condition in TriggerParentCondition.objects.filter(entity=entity):
             # This prevents to registering exactly same condition that have already been registered
             if parent_condition.is_match_condition(input_trigger_conditions):
-                raise InvalidInputException("There is condition that have same condition with specified one")
+                raise InvalidInputException(
+                    "There is condition that have same condition with specified one"
+                )
 
         # register specified condition as AirOne TriggerCondition
         parent_condition = TriggerParentCondition.objects.create(entity=entity)
         parent_condition.save_conditions(input_trigger_conditions)
 
         # todo: implement processing to register Trigger action instances associated with Trigger condition
-        pass
 
-
-class TriggerParentAction(models.Model):
-    condition = models.ForeignKey(
-        TriggerParentCondition, on_delete=models.CASCADE, related_name="action"
-    )
+        # convert input to InputTriggerCondition
+        for action in actions:
+            input_trigger_action = InputTriggerAction(**action)
+            trigger_action = TriggerAction.objects.create(
+                condition=parent_condition, attr=input_trigger_action.attr
+            )
+            trigger_action.save_actions(input_trigger_action)
 
 
 class TriggerAction(models.Model):
-    parent = models.ForeignKey(
-        TriggerParentAction, on_delete=models.CASCADE, related_name="co_action"
+    condition = models.ForeignKey(
+        TriggerParentCondition, on_delete=models.CASCADE, related_name="actions"
     )
     attr = models.ForeignKey("entity.EntityAttr", on_delete=models.CASCADE)
+
+    def save_actions(self, input: InputTriggerAction):
+        for input_action_value in input.values:
+            params = {
+                "action": self,
+                "str_cond": input_action_value.str_cond,
+                "ref_cond": input_action_value.ref_cond,
+                "bool_cond": input_action_value.bool_cond,
+            }
+            TriggerActionValue.objects.create(**params)
 
 
 class TriggerActionValue(models.Model):
     action = models.ForeignKey(TriggerAction, on_delete=models.CASCADE, related_name="values")
     str_cond = models.TextField()
-    ref_cond = models.ManyToManyField("entry.Entry", blank=True)
+    ref_cond = models.ForeignKey("entry.Entry", on_delete=models.SET_NULL, null=True, blank=True)
     bool_cond = models.BooleanField(default=False)
 
     # TODO: Add method to register value to Attribute when action is invoked
--- trigger/tests/test_models.py
+++ trigger/tests/test_models.py
@@ -1,134 +1,169 @@
-from airone.lib.types import AttrTypeValue
 from airone.lib.test import AironeTestCase
+from airone.lib.types import AttrTypeValue
 from trigger.models import (
-  TriggerParentCondition,
-  TriggerCondition,
-  TriggerParentAction,
-  TriggerAction,
-  TriggerActionValue,
-  InputAironeTriggerCondition,
-  InputAironeTriggerActionValue,
-  InputAironeTriggerAction,
+    InputTriggerAction,
+    InputTriggerActionValue,
+    InputTriggerCondition,
+    TriggerAction,
+    TriggerActionValue,
+    TriggerCondition,
+    TriggerParentCondition,
 )
 from user.models import User
 
 
 class ModelTest(AironeTestCase):
-  def setUp(self):
-    super(ModelTest, self).setUp()
+    def setUp(self):
+        super(ModelTest, self).setUp()
 
-    self.user: User = User.objects.create(username="test")
-    self.entity_ref = self.create_entity(self.user, "test_entity_ref")
-    self.entity = self.create_entity(self.user, "test_entity", attrs=[
-      # These are attributes that are used for trigger
-      {"name": "str_trigger", "type": AttrTypeValue["string"]},
-      {"name": "ref_trigger", "type": AttrTypeValue["object"], "ref": self.entity_ref},
-      {"name": "named_trigger", "type": AttrTypeValue["named_object"], "ref": self.entity_ref},
-      {"name": "arr_str_trigger", "type": AttrTypeValue["array_string"]},
-      {"name": "arr_ref_trigger", "type": AttrTypeValue["array_object"], "ref": self.entity_ref},
-      {"name": "arr_named_trigger", "type": AttrTypeValue["array_named_object"],
-       "ref": self.entity_ref},
-      {"name": "bool_trigger", "type": AttrTypeValue["boolean"]},
+        self.user: User = User.objects.create(username="test")
+        self.entity_ref = self.create_entity(self.user, "test_entity_ref")
+        self.entity = self.create_entity(
+            self.user,
+            "test_entity",
+            attrs=[
+                # These are attributes that are used for trigger
+                {"name": "str_trigger", "type": AttrTypeValue["string"]},
+                {"name": "ref_trigger", "type": AttrTypeValue["object"], "ref": self.entity_ref},
+                {
+                    "name": "named_trigger",
+                    "type": AttrTypeValue["named_object"],
+                    "ref": self.entity_ref,
+                },
+                {"name": "arr_str_trigger", "type": AttrTypeValue["array_string"]},
+                {
+                    "name": "arr_ref_trigger",
+                    "type": AttrTypeValue["array_object"],
+                    "ref": self.entity_ref,
+                },
+                {
+                    "name": "arr_named_trigger",
+                    "type": AttrTypeValue["array_named_object"],
+                    "ref": self.entity_ref,
+                },
+                {"name": "bool_trigger", "type": AttrTypeValue["boolean"]},
+                # These are attributes that are used for action
+                {"name": "str_action", "type": AttrTypeValue["string"]},
+                {"name": "ref_action", "type": AttrTypeValue["object"], "ref": self.entity_ref},
+                {
+                    "name": "named_action",
+                    "type": AttrTypeValue["named_object"],
+                    "ref": self.entity_ref,
+                },
+                {"name": "arr_str_action", "type": AttrTypeValue["array_string"]},
+                {
+                    "name": "arr_ref_action",
+                    "type": AttrTypeValue["array_object"],
+                    "ref": self.entity_ref,
+                },
+                {
+                    "name": "arr_named_action",
+                    "type": AttrTypeValue["array_named_object"],
+                    "ref": self.entity_ref,
+                },
+                {"name": "bool_action", "type": AttrTypeValue["boolean"]},
+            ],
+        )
 
-      # These are attributes that are used for action
-      {"name": "str_action", "type": AttrTypeValue["string"]},
-      {"name": "ref_action", "type": AttrTypeValue["object"], "ref": self.entity_ref},
-      {"name": "named_action", "type": AttrTypeValue["named_object"], "ref": self.entity_ref},
-      {"name": "arr_str_action", "type": AttrTypeValue["array_string"]},
-      {"name": "arr_ref_action", "type": AttrTypeValue["array_object"], "ref": self.entity_ref},
-      {"name": "arr_name_ref_action", "type": AttrTypeValue["array_named_object"],
-       "ref": self.entity_ref},
-      {"name": "bool_action", "type": AttrTypeValue["boolean"]},
-    ])
+    def test_input_airone_trigger_action(self):
+        entries = [self.add_entry(self.user, "test_entry_%s" % i, self.entity) for i in range(3)]
 
-  def test_create_condition_for_str(self):
-    # create Condition
-    parent_condition = TriggerParentCondition.objects.create(entity=self.entity)
-    cond_attr_str = TriggerCondition.objects.create(
-      parent=parent_condition,
-      attr=self.entity.attrs.get(name="str_trigger"),
-      str_cond="test",
-    )
+        TEST_CONDITIONS = [
+            {"attr_id": self.entity.attrs.get(name="str_trigger").id, "str_cond": "test"},
+            {"attr_id": self.entity.attrs.get(name="ref_trigger").id, "ref_cond": entries[0].id},
+            {
+                "attr_id": self.entity.attrs.get(name="named_trigger").id,
+                "str_cond": "test",
+                "ref_cond": entries[0].id,
+            },
+            {"attr_id": self.entity.attrs.get(name="arr_str_trigger").id, "str_cond": "test"},
+            {
+                "attr_id": self.entity.attrs.get(name="arr_ref_trigger").id,
+                "ref_cond": entries[0].id,
+            },
+            {
+                "attr_id": self.entity.attrs.get(name="arr_named_trigger").id,
+                "str_cond": "test",
+                "ref_cond": entries[0].id,
+            },
+            {"attr_id": self.entity.attrs.get(name="bool_trigger").id, "bool_cond": False},
+        ]
+        for cond in TEST_CONDITIONS:
+            input_trigger_condition = InputTriggerCondition(**cond)
+            self.assertEqual(input_trigger_condition.attr.id, cond["attr_id"])
+            self.assertEqual(input_trigger_condition.str_cond, cond.get("str_cond", ""))
+            self.assertEqual(input_trigger_condition.bool_cond, cond.get("bool_cond", False))
+            if cond.get("ref_cond", None):
+                self.assertEqual(input_trigger_condition.ref_cond.id, cond.get("ref_cond", None))
 
-    # create Action
-    parent_action = TriggerParentAction.objects.create(condition=parent_condition)
-    action = TriggerAction.objects.create(
-      parent=parent_action,
-      attr=self.entity.attrs.get(name="str_action"),
-    )
+    def test_create_trigger_action_initialization(self):
+        entries = [self.add_entry(self.user, "test_entry_%s" % i, self.entity) for i in range(3)]
 
-  def test_input_airone_trigger_action(self):
-    entries = [self.add_entry(self.user, "test_entry_%s" % i, self.entity) for i in range(3)]
+        # NOTE: This only assume partial match. When the specification is change to expand to support
+        #       full match, this test should be updated.
+        settingTriggerConditions = [
+            {"attr_id": self.entity.attrs.get(name="str_trigger").id, "str_cond": "test"},
+            {"attr_id": self.entity.attrs.get(name="ref_trigger").id, "ref_cond": entries[0].id},
+            {
+                "attr_id": self.entity.attrs.get(name="named_trigger").id,
+                "str_cond": "test",
+                "ref_cond": entries[0].id,
+            },
+            {"attr_id": self.entity.attrs.get(name="bool_trigger").id, "bool_cond": False},
+            {"attr_id": self.entity.attrs.get(name="arr_str_trigger").id, "str_cond": "test"},
+            {
+                "attr_id": self.entity.attrs.get(name="arr_ref_trigger").id,
+                "ref_cond": entries[0].id,
+            },
+            {
+                "attr_id": self.entity.attrs.get(name="arr_named_trigger").id,
+                "str_cond": "test",
+                "ref_cond": entries[0].id,
+            },
+        ]
+        settingTriggerActions = [
+            {"attr_id": self.entity.attrs.get(name="str_action").id, "value": "changed_value"},
+            {"attr_id": self.entity.attrs.get(name="ref_action").id, "value": entries[1].id},
+            {
+                "attr_id": self.entity.attrs.get(name="named_action").id,
+                "value": {"name": "changed_value", "id": entries[1].id},
+            },
+            {"attr_id": self.entity.attrs.get(name="bool_action").id, "value": True},
+            {
+                "attr_id": self.entity.attrs.get(name="arr_str_action").id,
+                "value": ["foo", "bar", "baz"],
+            },
+            {
+                "attr_id": self.entity.attrs.get(name="arr_ref_action").id,
+                "value": [x.id for x in entries],
+            },
+            {
+                "attr_id": self.entity.attrs.get(name="arr_named_action").id,
+                "value": [
+                    {"name": "foo", "id": entries[0].id},
+                    {"name": "bar", "id": None},
+                    {"name": "", "id": entries[2].id},
+                ],
+            },
+        ]
 
-    TEST_CONDITIONS = [
-      {"attr_id": self.entity.attrs.get(name="str_trigger").id, "str_cond": "test"},
-      {"attr_id": self.entity.attrs.get(name="ref_trigger").id, "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="named_trigger").id, "str_cond": "test", "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="arr_str_trigger").id, "str_cond": "test"},
-      {"attr_id": self.entity.attrs.get(name="arr_ref_trigger").id, "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="arr_named_trigger").id, "str_cond": "test", "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="bool_trigger").id, "bool_cond": False},
-    ]
-    for cond in TEST_CONDITIONS:
-      input_trigger_condition = InputAironeTriggerCondition(**cond)
-      self.assertEqual(input_trigger_condition.attr.id, cond["attr_id"])
-      self.assertEqual(input_trigger_condition.str_cond, cond.get("str_cond", ""))
-      self.assertEqual(input_trigger_condition.bool_cond, cond.get("bool_cond", False))
-      if cond.get("ref_cond", None):
-        self.assertEqual(input_trigger_condition.ref_cond.id, cond.get("ref_cond", None))
+        # This tests processing to initialize Airone Triggers and its Actions
+        TriggerCondition.register(self.entity, settingTriggerConditions, settingTriggerActions)
 
-  def test_trigger_parent_condition(self):
-    trigger_parent = TriggerParentCondition.objects.create(entity=self.entity)
+        # This checks expected Trigger condition objects would be created
+        parent_conditions = TriggerParentCondition.objects.filter(entity=self.entity)
+        self.assertEqual(parent_conditions.count(), 1)
 
+        trigger_conditions = TriggerCondition.objects.filter(parent=parent_conditions[0])
+        self.assertEqual(trigger_conditions.count(), len(settingTriggerConditions))
+        # omit tests for TriggerCondition context
 
-  def test_create_trigger_action_initialization(self):
-    entries = [self.add_entry(self.user, "test_entry_%s" % i, self.entity) for i in range(3)]
+        # This checks expected Trigger action objects would be created
+        trigger_actions = TriggerAction.objects.filter(condition=parent_conditions[0])
+        self.assertEqual(trigger_actions.count(), len(settingTriggerActions))
 
-    # NOTE: This only assume partial match. When the specification is change to expand to support
-    #       full match, this test should be updated.
-    settingTriggerConditions = [
-      {"attr_id": self.entity.attrs.get(name="str_trigger").id, "str_cond": "test"},
-      {"attr_id": self.entity.attrs.get(name="ref_trigger").id, "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="named_trigger").id, "str_cond": "test", "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="bool_trigger").id, "bool_cond": False},
-      {"attr_id": self.entity.attrs.get(name="arr_str_trigger").id, "str_cond": "test"},
-      {"attr_id": self.entity.attrs.get(name="arr_ref_trigger").id, "ref_cond": entries[0].id},
-      {"attr_id": self.entity.attrs.get(name="arr_named_trigger").id, "str_cond": "test", "ref_cond": entries[0].id},
-    ]
-    settingTriggerActions = [
-      {"attr_id": self.entity.attrs.get(name="str_action").id, "value": "changed_value"},
-      {"attr_id": self.entity.attrs.get(name="ref_action").id, "value": entries[1].id},
-      {"attr_id": self.entity.attrs.get(name="named_action").id, "value": {"name": "changed_value", "id": entries[1].id}},
-      {"attr_id": self.entity.attrs.get(name="bool_action").id, "value": True},
-      {"attr_id": self.entity.attrs.get(name="arr_str_action").id, "value": ["foo", "bar", "baz"]},
-      {"attr_id": self.entity.attrs.get(name="arr_ref_action").id, "value": [x.id for x in entries]},
-      {"attr_id": self.entity.attrs.get(name="arr_named_action").id, "value": [
-        {"name": "foo", "id": entries[0].id},
-        {"name": "bar", "id": None},
-        {"name": "", "id": entries[2].id},
-      ]},
-    ]
-
-    # This tests processing to initialize Airone Triggers and its Actions
-    TriggerCondition.register(self.entity, settingTriggerConditions, settingTriggerActions)
-
-    # This checks expected Trigger condition objects would be created
-    parent_conditions = TriggerParentCondition.objects.filter(entity=self.entity)
-    self.assertEqual(parent_conditions.count(), 1)
-
-    trigger_conditions = TriggerCondition.objects.filter(parent=parent_conditions[0])
-    self.assertEqual(trigger_conditions.count(), len(settingTriggerConditions))
-    # omit tests for TriggerCondition context
-
-    # This checks expected Trigger action objects would be created
-    parent_action = TriggerParentAction.objects.filter(condition=parent_conditions[0])
-    self.assertEqual(parent_conditions.count(), 1)
-
-    trigger_actions = TriggerAction.objects.filter(parent=parent_action[0])
-    self.assertEqual(trigger_actions.count(), len(settingTriggerActions))
-
-    for trigger_action in trigger_actions:
-      if trigger_action.attr.type & AttrTypeValue["array"]:
-        self.assertEqual(trigger_action.values.count(), 3)
-      else:
-        self.assertEqual(trigger_action.values.count(), 1)
+        for trigger_action in trigger_actions:
+            if trigger_action.attr.type & AttrTypeValue["array"]:
+                self.assertEqual(trigger_action.values.count(), 3)
+            else:
+                self.assertEqual(trigger_action.values.count(), 1)
