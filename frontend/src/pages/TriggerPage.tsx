74ca7b3838664610b6c1a0d9250cd5e06c9981fc
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2023-12-28 08:03:02 +0000

    Completed basic feature to show Trigger Conditions and Actions

--- frontend/src/pages/TriggerPage.tsx
+++ frontend/src/pages/TriggerPage.tsx
@@ -1,7 +1,14 @@
+import AddIcon from "@mui/icons-material/Add";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import {
   Box,
+  Button,
   Container,
   Divider,
+  IconButton,
+  List,
+  ListItem,
   Table,
   TableBody,
   TableCell,
@@ -11,15 +18,35 @@ import {
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
 import React, { FC, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { useAsync } from "react-use";
 
 import { topPath } from "Routes";
+import { useSnackbar } from "notistack";
 import { AironeBreadcrumbs } from "components/common/AironeBreadcrumbs";
+import { ExtendButtonBaseTypeMap } from "@mui/material/ButtonBase/ButtonBase";
+import { IconButtonTypeMap } from "@mui/material/IconButton/IconButton";
 import { Loading } from "components/common/Loading";
+import { OverridableComponent } from "@mui/material/OverridableComponent";
 import { PageHeader } from "components/common/PageHeader";
 import { usePage } from "hooks/usePage";
 import { aironeApiClientV2 } from "repository/AironeApiClientV2";
+
+import { Confirmable } from "components/common/Confirmable";
+import { editTriggerPath, triggersPath, newTriggerPath } from "Routes";
+import { TriggerAction, TriggerCondition } from "@dmm-com/airone-apiclient-typescript-fetch";
+
+const StyledList = styled(List)(() => ({
+  padding: "0",
+}));
+
+const StyledListItem = styled(ListItem)(() => ({
+  padding: "4px",
+}));
+
+const StyledIconButton = styled(IconButton)(({ theme }) => ({
+  margin: theme.spacing(1),
+})) as OverridableComponent<ExtendButtonBaseTypeMap<IconButtonTypeMap>>;
 
 const HeaderTableRow = styled(TableRow)(({ }) => ({
   backgroundColor: "#455A64",
@@ -44,9 +71,9 @@ const StyledBox = styled(Box)(() => ({
 }));
 
 const TriggerCondition: FC<{
-  cond: any;
+  cond: TriggerCondition;
 }> = ({ cond }) => {
-  return cond ? (
+  return (
     <StyledBox key={cond.id}>
       <Box>{cond.attr.name}</Box>
       <Divider orientation="vertical" flexItem />
@@ -54,21 +81,57 @@ const TriggerCondition: FC<{
         {cond.strCond}
       </Box>
     </StyledBox>
-  ) : (
-    <Box />
+  );
+};
+
+const TriggerAction: FC<{
+  action: TriggerAction;
+}> = ({ action }) => {
+  return (
+    <StyledBox key={action.id}>
+      <Box>{action.attr.name}</Box>
+      <Divider orientation="vertical" flexItem />
+      <Box>
+        {action.values.map((value) => {
+          return (
+            <Box>
+              {value.strCond}
+            </Box>
+          );
+        })}
+      </Box>
+    </StyledBox>
   );
 };
 
 export const TriggerPage: FC = () => {
+  const history = useHistory();
   const [page, changePage] = usePage();
   const [keyword, setKeyword] = useState("");
   const params = new URLSearchParams(location.search);
   const [query, setQuery] = useState<string>(params.get("query") ?? "");
   const [toggle, setToggle] = useState(false);
+  const { enqueueSnackbar } = useSnackbar();
 
   const triggers = useAsync(async () => {
     return await aironeApiClientV2.getTriggers(page);
   }, [page, query, toggle]);
+
+  const handleDelete = async (triggerId: number) => {
+    try {
+      await aironeApiClientV2.deleteRole(triggerId);
+      enqueueSnackbar(`トリガーの削除が完了しました`, {
+        variant: "success",
+      });
+      history.replace(topPath());
+      history.replace(triggersPath());
+      setToggle(!toggle);
+    } catch (e) {
+      enqueueSnackbar("トリガーの削除が失敗しました", {
+        variant: "error",
+      });
+    }
+  };
 
   return (
     <Box className="container-fluid">
@@ -78,7 +141,17 @@ export const TriggerPage: FC = () => {
         </Typography>
         <Typography color="textPrimary">トリガー管理</Typography>
       </AironeBreadcrumbs>
-      <PageHeader title="トリガー管理"></PageHeader>
+      <PageHeader title="トリガー管理">
+        <Button
+          variant="contained"
+          color="secondary"
+          component={Link}
+          to={newTriggerPath()}
+          sx={{ height: "48px", borderRadius: "24px", ml: "16px" }}
+        >
+          <AddIcon /> 新規トリガー/アクションを作成
+        </Button>
+      </PageHeader>
       <Container>
         <Box>
           {triggers.loading ? (
@@ -88,11 +161,13 @@ export const TriggerPage: FC = () => {
               <Table>
                 <TableHead>
                   <HeaderTableRow>
-                    <HeaderTableCell width="140px">
+                    <HeaderTableCell width="200px">
                       エンティティ
                     </HeaderTableCell>
-                    <HeaderTableCell width="140px">条件</HeaderTableCell>
-                    <HeaderTableCell width="140px">アクション</HeaderTableCell>
+                    <HeaderTableCell width="420px">条件</HeaderTableCell>
+                    <HeaderTableCell width="420px">アクション</HeaderTableCell>
+                    <HeaderTableCell width="20px"></HeaderTableCell>
+                    <HeaderTableCell width="20px"></HeaderTableCell>
                   </HeaderTableRow>
                 </TableHead>
                 <TableBody>
@@ -101,29 +176,51 @@ export const TriggerPage: FC = () => {
                       <StyledTableRow key={trigger.id}>
                         <TableCell>{trigger.entity.name}</TableCell>
                         <TableCell>
-                          {trigger.conditions?.map((condition) => {
-                            return (
-                              <TriggerCondition cond={condition} />
-                            );
-                          })}
+                          <StyledList>
+                            {trigger.conditions?.map((condition, n) => {
+                              return (
+                                <StyledListItem key={n}>
+                                  <TriggerCondition cond={condition} />
+                                </StyledListItem>
+                              );
+                            })}
+                          </StyledList>
                         </TableCell>
                         <TableCell>
-                          {trigger.actions.map((action) => {
-                            return (
-                              <Box key={action.id}>
-                                <Typography>{action.attr.name}</Typography>
-                                <Typography>
-                                  {action.values.map((value) => {
-                                    return (
-                                      <Box key={value.id}>
-                                        <Typography>{value.strCond}</Typography>
-                                      </Box>
-                                    );
-                                  })}
-                                </Typography>
-                              </Box>
-                            );
-                          })}
+                          <StyledList>
+                            {trigger.actions.map((action, n) => {
+                              return (
+                                <StyledListItem key={n}>
+                                  <TriggerAction action={action} />
+                                </StyledListItem>
+                              );
+                            })}
+                          </StyledList>
+                        </TableCell>
+
+                        {/* Delete TriggerParent */}
+                        <TableCell>
+                          <Confirmable
+                            componentGenerator={(handleOpen) => (
+                              <StyledIconButton
+                                onClick={handleOpen}
+                              >
+                                <DeleteOutlineIcon />
+                              </StyledIconButton>
+                            )}
+                            dialogTitle="本当に削除しますか？"
+                            onClickYes={() => handleDelete(trigger.id)}
+                          />
+                        </TableCell>
+
+                        {/* Edit TriggerParent */}
+                        <TableCell>
+                          <StyledIconButton
+                            component={Link}
+                            to={editTriggerPath(trigger.id)}
+                          >
+                            <EditOutlinedIcon />
+                          </StyledIconButton>
                         </TableCell>
                       </StyledTableRow>
                     );
@@ -133,7 +230,7 @@ export const TriggerPage: FC = () => {
             </>
           )}
         </Box>
-      </Container>
-    </Box>
+      </Container >
+    </Box >
   );
 };
