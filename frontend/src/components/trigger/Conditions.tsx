249cc63219a3792c779b0fa350a14090e63b930f
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2024-01-11 09:04:36 +0000

    (WIP) make processing to switch to show condition value component depending on Attribute type

--- frontend/src/components/trigger/Conditions.tsx
+++ frontend/src/components/trigger/Conditions.tsx
@@ -1,5 +1,6 @@
-import { EntityDetail } from "@dmm-com/airone-apiclient-typescript-fetch";
-import { MenuItem, Select, TableRow } from "@mui/material";
+import { EntryAttributeTypeTypeEnum } from "@dmm-com/airone-apiclient-typescript-fetch";
+import { EntityDetail, TriggerCondition } from "@dmm-com/airone-apiclient-typescript-fetch";
+import { MenuItem, Select, TableRow, TableCell, TextField } from "@mui/material";
 import React, { FC } from "react";
 import { Control, Controller, useFieldArray } from "react-hook-form";
 import { Schema } from "./TriggerFormSchema";
@@ -7,6 +8,71 @@ import { Schema } from "./TriggerFormSchema";
 interface Props {
   control: Control<Schema>;
   entity: EntityDetail;
+}
+
+interface PropsConditionValue {
+  index: number,
+  control: Control<Schema>,
+  field: TriggerCondition,
+  entity: EntityDetail;
+}
+
+interface PropsConditionValueComponent {
+  index: number,
+  control: Control<Schema>,
+  field: TriggerCondition,
+}
+
+const ConditionValueAsString: FC<PropsConditionValueComponent> = ({
+  index,
+  control,
+  field,
+}) => {
+  return (
+    <Controller
+      name={`conditions.${index}.strCond`}
+      control={control}
+      defaultValue={field.strCond}
+      render={({ field }) => (
+        <TextField {...field}
+          variant="standard"
+          fullWidth
+        />
+      )}
+    />
+  );
+}
+
+const ConditionValue: FC<PropsConditionValue> = ({
+  index,
+  control,
+  field,
+  entity,
+}) => {
+  // This is DEBUG code
+  const attrType = EntryAttributeTypeTypeEnum.STRING // TODO: get proper entity attr from entity object
+  switch (attrType) {
+    case EntryAttributeTypeTypeEnum.STRING:
+      return (
+        <ConditionValueAsString
+          index={index}
+          control={control}
+          field={field}
+        />
+      );
+
+    /* 
+    case EntryAttributeTypeTypeEnum.OBJECT:
+      return (
+        <></>
+      );
+
+    case EntryAttributeTypeTypeEnum.BOOLEAN:
+      return (
+        <></>
+      );
+    */
+  }
 }
 
 export const Conditions: FC<Props> = ({
@@ -20,25 +86,37 @@ export const Conditions: FC<Props> = ({
     keyName: "key", // NOTE: attr has 'id' field conflicts default key name
   });
 
+  console.log("[onix] entity: ", entity);
+
   return (
     <>
       {fields.map((field, index) => {
         return (
           <TableRow key={field.key}>
-            <Controller
-              name={`conditions.${index}.attr.id`}
-              control={control}
-              defaultValue={field.attr.id}
-              render={({ field }) => (
-                <Select {...field} size="small" fullWidth>
-                  {entity.attrs.map((attr) => (
-                    <MenuItem key={attr.id} value={attr.id}>
-                      {attr.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              )}
-            />
+            <TableCell>
+              <Controller
+                name={`conditions.${index}.attr.id`}
+                control={control}
+                defaultValue={field.attr.id}
+                render={({ field }) => (
+                  <Select {...field} size="small" fullWidth>
+                    {entity.attrs.map((attr) => (
+                      <MenuItem key={attr.id} value={attr.id}>
+                        {attr.name}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                )}
+              />
+            </TableCell>
+            <TableCell>
+              <ConditionValue
+                index={index}
+                control={control}
+                field={field}
+                entity={entity}
+              />
+            </TableCell>
           </TableRow>
         );
       })}
