a7ce1ad02ac83f4ff959b285bcf5003a6a9105d9
Ryo Okubo <syucream1031@gmail.com>
2024-01-20 16:22:48 +0900

    Use typescript-eslint 6

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,7 @@ module.exports = {
     ],
     "no-unused-vars": "off",
     "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "warn",
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
