949aaabe568f554fa0f4c175521cef012c1d7e48
Ryo Okubo <syucream1031@gmail.com>
2024-02-03 17:48:08 +0900

    Add tests for trigger pages

---
+++ frontend/src/pages/TriggerPage.test.tsx
@@ -0,0 +1,73 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {
+  render,
+  waitForElementToBeRemoved,
+  screen,
+} from "@testing-library/react";
+import { http, HttpResponse } from "msw";
+import { setupServer } from "msw/node";
+import React from "react";
+
+import { TriggerPage } from "./TriggerPage";
+
+import { TestWrapper } from "TestWrapper";
+
+const server = setupServer(
+  // getTriggers
+  http.get("http://localhost/trigger/api/v2/", () => {
+    return HttpResponse.json({
+      count: 2,
+      next: null,
+      previous: null,
+      results: [
+        {
+          id: 1,
+          entity: {
+            id: 1,
+            name: "test entity1",
+            note: "",
+            isToplevel: false,
+            attrs: [],
+            webhooks: [],
+          },
+          actions: [],
+          conditions: [],
+        },
+        {
+          id: 2,
+          entity: {
+            id: 2,
+            name: "test entity2",
+            note: "",
+            isToplevel: false,
+            attrs: [],
+            webhooks: [],
+          },
+          actions: [],
+          conditions: [],
+        },
+      ],
+    });
+  })
+);
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
+
+describe("TriggerPage", () => {
+  test("should match snapshot", async () => {
+    // wait async calls and get rendered fragment
+    const result = render(<TriggerPage />, {
+      wrapper: TestWrapper,
+    });
+    await waitForElementToBeRemoved(screen.getByTestId("loading"));
+
+    expect(result).toMatchSnapshot();
+  });
+});
