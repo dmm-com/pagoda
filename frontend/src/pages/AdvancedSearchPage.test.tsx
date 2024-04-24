3944dafecd6b212dca8e8c37dcfc44f3a4ac4022
hinashi <k.hinagawa@gmail.com>
2024-04-24 13:55:56 +0900

    FIxed selected entity test in advanced search page

--- frontend/src/pages/AdvancedSearchPage.test.tsx
+++ frontend/src/pages/AdvancedSearchPage.test.tsx
@@ -7,7 +7,7 @@ import {
   EntryAttributeTypeTypeEnum,
   PaginatedEntityListList,
 } from "@dmm-com/airone-apiclient-typescript-fetch";
-import { act, render, screen, fireEvent } from "@testing-library/react";
+import { act, fireEvent, render, screen } from "@testing-library/react";
 import { HttpResponse, http } from "msw";
 import { setupServer } from "msw/node";
 import React from "react";
@@ -49,10 +49,12 @@ const entityAttrs: Array<EntityAttr> = [
 
 const server = setupServer(
   http.get("http://localhost/entity/api/v2/", () => {
+    console.log("http://localhost/entity/api/v2/");
     return HttpResponse.json(entities);
   }),
 
   http.get("http://localhost/entity/api/v2/attrs", () => {
+    console.log("http://localhost/entity/api/v2/attrs");
     return HttpResponse.json(entityAttrs);
   })
 );
@@ -64,7 +66,7 @@ afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
 
 describe("AdvancedSearchPage", () => {
-  test("should render the component", async () => {
+  test("selected entity test", async () => {
     await act(async () => {
       render(
         <Router>
@@ -75,29 +77,45 @@ describe("AdvancedSearchPage", () => {
 
     const elemInputEntity = screen.getByPlaceholderText("エンティティを選択");
 
-    elemInputEntity.click();
+    if (elemInputEntity.parentNode) {
+      fireEvent.click(elemInputEntity.parentNode);
+    } else {
+      throw new Error("Parent node not found");
+    }
 
-    //console.debug(screen.getAllByRole("option"));
-
-    // NG pattern - that uses userEvent
-    //userEvent.type(screen.getByRole("combobox"), "entity");
-
-    // OK pattern1
-    //fireEvent.change(screen.getByRole("combobox"), { target: { value: "entity" } });
+    let options = screen.getAllByRole("option");
+    expect(options).toHaveLength(2);
+    expect(options[0]).toHaveTextContent("entity1");
+    expect(options[1]).toHaveTextContent("entity2");
 
     // write down text to the input field
-    fireEvent.change(elemInputEntity, { target: { value: "entity" } });
-    expect(elemInputEntity).toHaveValue("entity");
+    fireEvent.change(elemInputEntity, { target: { value: "2" } });
+    expect(elemInputEntity).toHaveValue("2");
 
     // check expected values that are retrieved from API will be displaied.
-    screen.debug();
+    options = screen.getAllByRole("option");
+    expect(options).toHaveLength(1);
+    expect(options[0]).toHaveTextContent("entity2");
 
-    //expect(elemInputEntity).toHaveValue("entity");
+    fireEvent.change(elemInputEntity, { target: { value: "hoge" } });
+    expect(screen.getByText("No options")).toBeInTheDocument();
 
-    //console.debug(elemInputEntity);
+    options = screen.queryAllByRole("option");
+    expect(options).toHaveLength(0);
 
+    //screen.debug();
 
-    //const selectElement = screen.getByRole("combobox");
+    const elemInputEntityAttr = screen.getByPlaceholderText("属性を選択");
 
+    if (elemInputEntityAttr.parentNode) {
+      fireEvent.click(elemInputEntityAttr.parentNode);
+    } else {
+      throw new Error("Parent node not found");
+    }
+
+    options = screen.getAllByRole("option");
+    expect(options).toHaveLength(2);
+    expect(options[0]).toHaveTextContent("str");
+    expect(options[1]).toHaveTextContent("obj");
   });
 });
