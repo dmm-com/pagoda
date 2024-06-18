8ba923eecea02ff60e870d4b40a88267091d9995
角田 峻宏@inf-sog <tsunoda-takahiro@dmm.com>
2024-06-18 16:30:40 +0900

    Enhanced date validation to support both YYYY-MM-DD and YYYY/MM/DD formats

--- api_v1/serializers.py
+++ api_v1/serializers.py
@@ -187,11 +187,15 @@ class PostEntrySerializer(serializers.Serializer):
 
         elif attr.type & AttrType.DATE:
             if isinstance(value, str):
-                try:
-                    datetime.strptime(value, "%Y-%m-%d")
-                except ValueError:
-                    raise ValueError("Incorrect data format, should be YYYY-MM-DD")
-                return datetime.strptime(value, "%Y-%m-%d")
+                date_formats = ["%Y-%m-%d", "%Y/%m/%d"]  # List of acceptable date formats
+                for date_format in date_formats:
+                    try:
+                        parsed_date = datetime.strptime(value, date_format)
+                        return parsed_date
+                    except ValueError:
+                        continue # Try the next format
+                # If all formats failed, raise an error
+                raise ValidationError("Incorrect data format, should be YYYY-MM-DD or YYYY/MM/DD")
             else:
                 return None
 
