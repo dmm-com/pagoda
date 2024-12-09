4829ffc88f0070bbf1336ea99556405f7b75c01c
Hiroyasu OHYAMA <user.localhost2000@gmail.com>
2024-12-09 08:01:08 +0000

    Fixed problem that might raise ElasticsearchException depends on context by SearchChain processing

--- api_v1/entry/serializer.py
+++ api_v1/entry/serializer.py
@@ -302,14 +302,21 @@ class EntrySearchChainSerializer(serializers.Serializer):
             ]
 
             # get Entry informations from result
+            # NOTE: small limit(=1000) is workaround to prevent overload
             try:
                 search_result = AdvancedSearchService.search_entries(
-                    user, entity_id_list, hint_attrs, limit=99999
+                    user,
+                    entity_id_list,
+                    hint_attrs,
+                    limit=CONFIG.SEARCH_CHAIN_ACCEPTABLE_RESULT_COUNT,
                 )
             except Exception as e:
                 Logger.warning("Search Chain API error:%s" % e)
                 raise ElasticsearchException()
 
+            # All results of this request would be joined and pass to next request. It might be
+            # huge request and leads to glitch of Elasticsearch by just a single request.
+            # This is our original curcit breaker to prevent overload because of them.
             if search_result.ret_count > CONFIG.SEARCH_CHAIN_ACCEPTABLE_RESULT_COUNT:
                 Logger.warning("Search Chain API error: SEARCH_CHAIN_ACCEPTABLE_RESULT_COUNT")
                 raise ElasticsearchException()
