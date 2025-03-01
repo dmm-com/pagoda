9a8a9d1b5a8f31f54fb0b5a079e46f62dd9adf57
Ryo Okubo <syucream1031@gmail.com>
2025-03-01 10:55:44 +0900

    chore: Configure Jest for React testing with MSW v2 support
    Update Jest configuration to:
    - Set test environment to jsdom
    - Add polyfills for MSW v2 compatibility
    - Define global web APIs and BroadcastChannel mock

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -13,20 +13,58 @@ Object.defineProperties(globalThis, {
     clearImmediate: { value: clearImmediate },
 });
 
-const { ReadableStream } = require('node:stream/web');
+const { ReadableStream, TransformStream } = require('node:stream/web');
 if (globalThis.ReadableStream === undefined) {
     globalThis.ReadableStream = ReadableStream
 }
+if (globalThis.TransformStream === undefined) {
+    globalThis.TransformStream = TransformStream
+}
 
+// Add required web API globals for MSW
 const { Blob, File } = require('node:buffer');
 const { fetch, Headers, FormData, Request, Response } = require('undici');
 
-Object.defineProperties(globalThis, {
-    fetch: { value: fetch, writable: true },
-    Blob: { value: Blob },
-    File: { value: File },
-    Headers: { value: Headers },
-    FormData: { value: FormData },
-    Request: { value: Request },
-    Response: { value: Response },
-});
+// Define these globals for MSW
+globalThis.fetch = fetch;
+globalThis.Blob = Blob;
+globalThis.File = File;
+globalThis.Headers = Headers;
+globalThis.FormData = FormData;
+globalThis.Request = Request;
+globalThis.Response = Response;
+
+// MSW v2 requires BroadcastChannel
+class BroadcastChannel {
+  constructor(name) {
+    this.name = name;
+    this.listeners = {};
+  }
+
+  postMessage(message) {
+    // Mock implementation
+  }
+
+  addEventListener(type, listener) {
+    if (!this.listeners[type]) {
+      this.listeners[type] = [];
+    }
+    this.listeners[type].push(listener);
+  }
+
+  removeEventListener(type, listener) {
+    if (!this.listeners[type]) {
+      return;
+    }
+    this.listeners[type] = this.listeners[type].filter(l => l !== listener);
+  }
+
+  close() {
+    this.listeners = {};
+  }
+}
+
+// Add BroadcastChannel to global scope
+if (!globalThis.BroadcastChannel) {
+    globalThis.BroadcastChannel = BroadcastChannel;
+}
