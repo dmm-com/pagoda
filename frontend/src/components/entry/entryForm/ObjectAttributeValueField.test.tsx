b23c56d0bbb21dcb24e117da54992ff3db08725f
Ryo Okubo <syucream1031@gmail.com>
2025-04-19 16:59:49 +0900

    Initialize array values in entry form fields and remove useEffect hooks

--- frontend/src/components/entry/entryForm/ObjectAttributeValueField.test.tsx
+++ frontend/src/components/entry/entryForm/ObjectAttributeValueField.test.tsx
@@ -93,7 +93,7 @@ describe("ObjectAttributeValueField", () => {
           name: "array-named-object",
         },
         value: {
-          asArrayNamedObject: [],
+          asArrayNamedObject: [{ name: "", object: null, _boolean: false }],
         },
       },
     },
